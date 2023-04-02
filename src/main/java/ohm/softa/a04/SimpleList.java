3ec1871f325c0f41a382ae224e7c21c0eb143e44
RadicalRussian <markuskostrizki@web.de>
2023-04-02 12:01:58 +0200

    My Try on generics assignment

--- src/main/java/ohm/softa/a04/SimpleList.java
+++ src/main/java/ohm/softa/a04/SimpleList.java
@@ -1,19 +1,59 @@
 package ohm.softa.a04;
 
-public interface SimpleList extends Iterable {
+import java.util.function.Function;
+public interface SimpleList<K> extends Iterable<K> {
 	/**
 	 * Add a given object to the back of the list.
 	 */
-	void add(Object o);
+	void add(K o);
 
 	/**
 	 * @return current size of the list
 	 */
 	int size();
 
+
+
+	default void addDefault (Class<K> clazz){
+		try{
+			this.add(clazz.newInstance());
+		}catch (InstantiationException | IllegalAccessException e){
+			e.printStackTrace();
+		}
+	}
+
 	/**
 	 * Generate a new list using the given filter instance.
 	 * @return a new, filtered list
 	 */
-	SimpleList filter(SimpleFilter filter);
+	default SimpleList<K> filter(SimpleFilter<K> filter){
+		SimpleList<K> result;
+		try{
+			result = (SimpleList<K>) getClass().newInstance();
+		}catch (InstantiationException | IllegalAccessException e){
+			result = new SimpleListImpl<>();
+		}
+
+		for(K o : this) {
+			if (filter.include(o)) {
+				result.add(o);
+			}
+		}
+		return result;
+
+	}
+
+	default <R> SimpleList<R> map(Function<K, R> transform){
+		SimpleList<R> result;
+		try {
+			result = (SimpleList<R>) this.getClass().newInstance();
+		} catch (InstantiationException | IllegalAccessException e) {
+			result = new SimpleListImpl<>();
+		}
+
+		for(K o : this){
+			result.add(transform.apply(o));
+		}
+		return result;
+	}
 }
